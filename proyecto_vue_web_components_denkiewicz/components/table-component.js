a6313ad6eeb9218efb8eba66794529c872125e04
nataliamd11 <nataliamdenk11@gmail.com>
2022-11-29 13:12:24 -0300

    arreglo - title en key

--- proyecto_vue_web_components_denkiewicz/components/table-component.js
+++ proyecto_vue_web_components_denkiewicz/components/table-component.js
@@ -4,7 +4,7 @@ Vue.component('table-component', {
               <h2>{{ tableContent.tableTitle }}</h2>
                 <table :class="tableStyle">
                 <tr>
-                <th v-for="(title, index) in tableContent.colTitle" :key="colTitle">{{ title }}</th>
+                <th v-for="(title, index) in tableContent.colTitle" :key="title">{{ title }}</th>
               </tr>
               <row-component v-for="(row, index) in tableContent.content" :key="index" :rowContent="row"></row-component>
                 </table>
