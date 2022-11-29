52cd1d2a500a1552beea2f6d48524b212be253fe
nataliamd11 <nataliamdenk11@gmail.com>
2022-11-29 13:10:09 -0300

    componente tabla

---
+++ proyecto_vue_web_components_denkiewicz/components/table-component.js
@@ -0,0 +1,12 @@
+Vue.component('table-component', {
+  props: ['tableContent', 'tableStyle',],
+  template: `<div>
+              <h2>{{ tableContent.tableTitle }}</h2>
+                <table :class="tableStyle">
+                <tr>
+                <th v-for="(title, index) in tableContent.colTitle" :key="colTitle">{{ title }}</th>
+              </tr>
+              <row-component v-for="(row, index) in tableContent.content" :key="index" :rowContent="row"></row-component>
+                </table>
+            </div>`
+})
