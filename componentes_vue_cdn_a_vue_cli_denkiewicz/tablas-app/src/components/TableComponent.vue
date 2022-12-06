1b9a22708d471ad2b08ba82ccb581f8d25b823ed
nataliamd11 <nataliamdenk11@gmail.com>
2022-12-06 11:41:59 -0300

    componente tabla

---
+++ componentes_vue_cdn_a_vue_cli_denkiewicz/tablas-app/src/components/TableComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>{{ tableContent.tableTitle }}</h2>
+    <table :class="tableStyle">
+      <tr>
+        <th v-for="title in tableContent.colTitle" :key="title">
+          {{ title }}
+        </th>
+      </tr>
+      <RowComponent
+        v-for="(row, index) in tableContent.content"
+        :key="index"
+        :rowContent="row"
+      ></RowComponent>
+    </table>
+  </div>
+</template>
+
+<script>
+import RowComponent from '@/components/RowComponent.vue';
+
+export default {
+  name: "TableComponent",
+  props: [
+    'tableContent',
+    'tableStyle',
+  ],
+  components: {
+    RowComponent,
+  },
+};
+</script>
