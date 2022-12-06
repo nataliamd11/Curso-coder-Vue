933d47509ba44ee75d6e7240dc12a1bf7d02fdef
nataliamd11 <nataliamdenk11@gmail.com>
2022-12-06 11:42:10 -0300

    componente fila

---
+++ componentes_vue_cdn_a_vue_cli_denkiewicz/tablas-app/src/components/RowComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <tr>
+    <td v-for="(cellItem, index) in rowContent" :key="index">{{ cellItem }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "RowComponent",
+  props: [
+    'rowContent',
+  ],
+};
+</script>
+
+<style>
+</style>
