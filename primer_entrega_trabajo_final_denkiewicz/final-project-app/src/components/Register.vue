e250ff32d8a4bd8e32771536cf702f63e5a28eac
nataliamd11 <nataliamdenk11@gmail.com>
2022-12-12 22:22:58 -0300

    estilo en register component

--- primer_entrega_trabajo_final_denkiewicz/final-project-app/src/components/Register.vue
+++ primer_entrega_trabajo_final_denkiewicz/final-project-app/src/components/Register.vue
@@ -2,56 +2,58 @@
   <div>
     <div class="row">
       <div class="col-3"></div>
-      <div class="col-6">
-        <h2>Cree su usuario</h2>
-        <form>
-          <div class="mb-3">
-            <label class="form-label">Nombre</label>
-            <input
-              type="text"
-              v-model="name"
-              class="form-control"
-              placeholder="Ingrese su nombre"
-            />
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Apellido</label>
-            <input
-              type="text"
-              v-model="surname"
-              class="form-control"
-              placeholder="Ingrese su apellido"
-            />
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Email</label>
-            <input
-              type="email"
-              v-model="email"
-              class="form-control"
-              placeholder="Ingrese su email"
-            />
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Contraseña</label>
-            <input
-              type="password"
-              v-model="password"
-              class="form-control"
-              placeholder="Ingrese su contraseña"
-            />
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Repita su contraseña</label>
-            <input
-              type="password"
-              v-model="password2"
-              class="form-control"
-              placeholder="Ingrese su contraseña"
-            />
-          </div>
-          <button type="submit" class="btn btn-primary">Registrarse</button>
-        </form>
+      <div class="col-6 borderComponent">
+        <div class="m-3">
+          <h2>Cree su usuario</h2>
+          <form>
+            <div class="mb-3">
+              <label class="form-label">Nombre</label>
+              <input
+                type="text"
+                v-model="name"
+                class="form-control"
+                placeholder="Ingrese su nombre"
+              />
+            </div>
+            <div class="mb-3">
+              <label class="form-label">Apellido</label>
+              <input
+                type="text"
+                v-model="surname"
+                class="form-control"
+                placeholder="Ingrese su apellido"
+              />
+            </div>
+            <div class="mb-3">
+              <label class="form-label">Email</label>
+              <input
+                type="email"
+                v-model="email"
+                class="form-control"
+                placeholder="Ingrese su email"
+              />
+            </div>
+            <div class="mb-3">
+              <label class="form-label">Contraseña</label>
+              <input
+                type="password"
+                v-model="password"
+                class="form-control"
+                placeholder="Ingrese su contraseña"
+              />
+            </div>
+            <div class="mb-3">
+              <label class="form-label">Repita su contraseña</label>
+              <input
+                type="password"
+                v-model="password2"
+                class="form-control"
+                placeholder="Ingrese su contraseña"
+              />
+            </div>
+            <button type="submit" class="btn btn-primary">Registrarse</button>
+          </form>
+        </div>
       </div>
       <div class="col-3"></div>
     </div>
