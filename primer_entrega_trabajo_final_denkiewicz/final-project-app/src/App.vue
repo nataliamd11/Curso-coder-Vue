d4edc315ba16be4b3463b859838b79c640075682
nataliamd11 <nataliamdenk11@gmail.com>
2022-12-12 23:38:26 -0300

    app con componentes

--- primer_entrega_trabajo_final_denkiewicz/final-project-app/src/App.vue
+++ primer_entrega_trabajo_final_denkiewicz/final-project-app/src/App.vue
@@ -1,27 +1,34 @@
 <template>
   <div id="app">
     <register-component></register-component>
-    <br/>
+    <br />
     <login-component></login-component>
+    <br />
+    <carrito-compras></carrito-compras>
+    <br />
+    <product-information></product-information>
   </div>
 </template>
 
 <script>
 import RegisterComponent from "@/components/Register.vue";
 import LoginComponent from "@/components/Login.vue";
-
+import CarritoCompras from "@/components/CarritoCompras.vue";
+import ProductInformation from "@/components/ProductInformation.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     RegisterComponent,
     LoginComponent,
-  }
-}
+    CarritoCompras,
+    ProductInformation,
+  },
+};
 </script>
 
 <style>
-#app {
+CarritoCompras#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -31,8 +38,8 @@ export default {
 }
 
 .borderComponent {
-    border-style: solid;
-    border-color: rgb(202, 198, 198);
-    border-width: 2px;
-  }
+  border-style: solid;
+  border-color: rgb(202, 198, 198);
+  border-width: 2px;
+}
 </style>
