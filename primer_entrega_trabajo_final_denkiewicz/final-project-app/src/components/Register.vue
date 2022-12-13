96748a5a6893f500c177bab9faaff9cb6077eb64
nataliamd11 <nataliamdenk11@gmail.com>
2022-12-12 22:09:06 -0300

    componente registro usuarios

---
+++ primer_entrega_trabajo_final_denkiewicz/final-project-app/src/components/Register.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-3"></div>
+      <div class="col-6">
+        <h2>Cree su usuario</h2>
+        <form>
+          <div class="mb-3">
+            <label class="form-label">Nombre</label>
+            <input
+              type="text"
+              v-model="name"
+              class="form-control"
+              placeholder="Ingrese su nombre"
+            />
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Apellido</label>
+            <input
+              type="text"
+              v-model="surname"
+              class="form-control"
+              placeholder="Ingrese su apellido"
+            />
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Email</label>
+            <input
+              type="email"
+              v-model="email"
+              class="form-control"
+              placeholder="Ingrese su email"
+            />
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Contraseña</label>
+            <input
+              type="password"
+              v-model="password"
+              class="form-control"
+              placeholder="Ingrese su contraseña"
+            />
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Repita su contraseña</label>
+            <input
+              type="password"
+              v-model="password2"
+              class="form-control"
+              placeholder="Ingrese su contraseña"
+            />
+          </div>
+          <button type="submit" class="btn btn-primary">Registrarse</button>
+        </form>
+      </div>
+      <div class="col-3"></div>
+    </div>
+    <p>{{ name }}</p>
+    <p>{{ surname }}</p>
+    <p>{{ email }}</p>
+    <p>{{ password }}</p>
+    <p>{{ password2 }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RegisterComponent",
+  data: function () {
+    return {
+      name: "",
+      surname: "",
+      email: "",
+      password: "",
+      password2: "",
+    };
+  },
+};
+</script>
